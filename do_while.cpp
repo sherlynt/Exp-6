6976324e242c09774edf3ac51a029ed6b988c0ef
sherlynt <tiwarisherlyn@gmail.com>
2024-08-07 20:49:05 +0530

    Create do_while.cpp

---
+++ do_while.cpp
@@ -0,0 +1,15 @@
+//Sherlyn Tiwari
+//PRN 23070123119
+//Exp 6 Do While Loop
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0;
+    do{
+        cout<<i<<endl;
+        i++;
+    }
+        while(i<10);
+        return 0;
+           
+}
