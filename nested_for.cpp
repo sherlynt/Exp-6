823ff5f65f1f26bc1ea3f50171c6df0b70159330
sherlynt <tiwarisherlyn@gmail.com>
2024-08-07 20:51:42 +0530

    Create nested_for.cpp

---
+++ nested_for.cpp
@@ -0,0 +1,15 @@
+//Sherlyn Tiwari
+//PRN 23070123119
+//Exp 6 Nested For Loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            cout << i * j<<"\t"; 
+        }
+        cout << endl;
+    }
+    return 0;
+}
